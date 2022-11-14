7aa210082cf669cc28f3109c245601db47a27441
Daniel Voigt <yidaotus@protonmail.com>
2022-11-14 20:17:30 +0100

    Move to psql

--- prisma/migrations/20221114165517_add_document/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Document" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "serializedDocument" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
